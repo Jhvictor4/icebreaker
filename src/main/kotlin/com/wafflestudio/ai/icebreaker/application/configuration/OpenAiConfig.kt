8748f6a6e9e4ae81b31206a942d90e60c4b3d9bb
강지혁 <wlgur7238@snu.ac.kr>
2024-02-12 23:44:31 +0900

    openAi client 변경
    Update OpenAiConfig.kt

--- src/main/kotlin/com/wafflestudio/ai/icebreaker/application/configuration/OpenAiConfig.kt
+++ src/main/kotlin/com/wafflestudio/ai/icebreaker/application/configuration/OpenAiConfig.kt
@@ -3,13 +3,10 @@ package com.wafflestudio.ai.icebreaker.application.configuration
 import com.aallam.openai.api.http.Timeout
 import com.aallam.openai.client.OpenAI
 import com.aallam.openai.client.RetryStrategy
-import io.ktor.client.engine.cio.*
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.security.cert.X509Certificate
-import javax.net.ssl.X509TrustManager
-import kotlin.time.Duration.Companion.seconds
+import kotlin.time.Duration.Companion.minutes
 
 @Configuration
 internal class OpenAiConfig(
@@ -21,22 +18,10 @@ internal class OpenAiConfig(
     fun openAiClient(): OpenAI {
         return OpenAI(
             token = apiKey,
-            timeout = Timeout(socket = 10.seconds),
+            timeout = Timeout(socket = 1.minutes),
             retry = RetryStrategy(maxRetries = 0),
             httpClientConfig = {
                 followRedirects = true
-                engine {
-                    this as CIOEngineConfig
-                    https {
-                        trustManager = object : X509TrustManager {
-                            override fun checkClientTrusted(p0: Array<out X509Certificate>?, p1: String?) { }
-
-                            override fun checkServerTrusted(p0: Array<out X509Certificate>?, p1: String?) { }
-
-                            override fun getAcceptedIssuers(): Array<X509Certificate>? = null
-                        }
-                    }
-                }
             }
         )
     }
