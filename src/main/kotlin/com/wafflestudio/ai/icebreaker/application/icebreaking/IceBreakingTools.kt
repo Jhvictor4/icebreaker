547247508ff75c09a8e00a5f4c205c84fc09a880
강지혁 <wlgur7238@snu.ac.kr>
2024-02-13 00:37:40 +0900

    IceBreakingTools -> Tool 변환 추가

--- src/main/kotlin/com/wafflestudio/ai/icebreaker/application/icebreaking/IceBreakingTools.kt
+++ src/main/kotlin/com/wafflestudio/ai/icebreaker/application/icebreaking/IceBreakingTools.kt
@@ -1,52 +1,47 @@
 package com.wafflestudio.ai.icebreaker.application.icebreaking
 
-import com.wafflestudio.ai.icebreaker.application.saju.SaJuService
-import com.wafflestudio.ai.icebreaker.application.user.BasicInformationMatch
-import com.wafflestudio.ai.icebreaker.application.user.UserInformation
-import kotlin.reflect.KClass
+import com.aallam.openai.api.chat.FunctionTool
+import com.aallam.openai.api.chat.Tool
+import com.aallam.openai.api.chat.ToolType
+import com.aallam.openai.api.core.Parameters
+import kotlinx.serialization.json.*
 
-typealias Clue = String
-
-enum class IceBreakingTools(
-    val informationType: KClass<out UserInformation>,
-    val function: IceBreakingToolFactory.(BasicInformationMatch) -> Clue
-) {
-    FETCH_SAJU(
-        UserInformation.Birthday::class,
-        { birth -> getSaJu(birth) }
-    ),
-    SEARCH_KEYWORD(
-        UserInformation.UnderstandingInformation::class,
-        { "SEARCH_KEYWORD" }
-    );
-
-    companion object {
-        fun asPrompt(): String {
-            return values()
-                .groupBy { it.informationType }
-                .map { (type, tools) ->
-                    "{\"${type.simpleName}\": [${tools.joinToString(",") { "\"${it.name}\"" }}]"
-                }.joinToString("\n")
+enum class IceBreakingTools {
+    FORTUNE_TELLING {
+        override fun toChatGptTool(): Tool {
+            return Tool(
+                ToolType.Function,
+                "can fetch 사주팔자 of each user based on their birthday, and interpretation of it.",
+                FunctionTool(
+                    name = "getSaJu",
+                    parameters = Parameters.Empty
+                )
+            )
         }
-    }
-}
+    },
+    SEARCH_KEYWORD {
+        override fun toChatGptTool(): Tool {
+            return Tool(
+                ToolType.Function,
+                "can fetch more information about the user with certain keyword.",
+                FunctionTool(
+                    name = "searchByKeyword",
+                    parameters = Parameters.buildJsonObject {
+                        put("type", "object")
+                        putJsonObject("properties") {
+                            putJsonObject("keyword") {
+                                put("type", "string")
+                                put("description", "The keyword to search for")
+                            }
+                        }
+                        putJsonArray("required") {
+                            add("keyword")
+                        }
+                    }
+                )
+            )
+        }
+    };
 
-object IceBreakingToolFactory {
-
-    private lateinit var saJuService: SaJuService
-
-    fun init(
-        saJuService: SaJuService
-    ) {
-        IceBreakingToolFactory.saJuService = saJuService
-    }
-
-    fun getSaJu(birth: BasicInformationMatch): Clue {
-        val birthdayA = birth.userA as UserInformation.Birthday
-        val birthdayB = birth.userB as UserInformation.Birthday
-        return """
-            userA 사주 : ${saJuService.getSaJu(birthdayA.date)}
-            userB 사주 : ${saJuService.getSaJu(birthdayB.date)}
-        """.trimIndent()
-    }
+    abstract fun toChatGptTool(): Tool
 }
