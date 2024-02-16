145dd7e6a4fe91b3dcd49356e93a62c7fcc76418
심영인 <sspjyins@snu.ac.kr>
2024-02-16 23:37:35 +0900

    API 더미 데이터 응답

--- src/main/kotlin/com/wafflestudio/ai/icebreaker/api/ApiController.kt
+++ src/main/kotlin/com/wafflestudio/ai/icebreaker/api/ApiController.kt
@@ -1,11 +1,12 @@
 package com.wafflestudio.ai.icebreaker.api
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.wafflestudio.ai.icebreaker.application.Log
 import com.wafflestudio.ai.icebreaker.application.common.ChatGptResponseDto
 import com.wafflestudio.ai.icebreaker.application.understanding.UnderstandingUseCase
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.wafflestudio.ai.icebreaker.application.user.UserInformation
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/v1")
@@ -13,9 +14,20 @@ class ApiController(
     private val useCase: UnderstandingUseCase
 ) {
 
-    data class UnderstandByUriRequest(
-        val uri: String
-    )
+    @GetMapping("/user")
+    fun getUser(): UserResponse {
+        return UserResponse(
+            basicInformation = BasicInformation(
+                name = "지혁",
+                birthDay = LocalDateTime.now(),
+                gender = UserInformation.Gender.MALE,
+                mbti = UserInformation.MBTI.ENTJ,
+                major = "컴퓨터공학부",
+            ),
+            imageUrls = listOf("https://some.domain.name/path"),
+            snsInformation = SnsInformation()
+        )
+    }
 
     @PostMapping("/understanding/by-uri")
     fun understand(
@@ -23,4 +35,48 @@ class ApiController(
     ): ChatGptResponseDto? {
         return useCase.understandByUri(request.uri)
     }
+
+    @PostMapping("/user/login")
+    fun login(): LoginResponse {
+        // TODO: session id 생성 및 저장
+        return LoginResponse("session-id")
+    }
+
+    @PostMapping("/user/basicInformation")
+    fun addBasicInformation(
+        @RequestBody basicInformation: BasicInformation,
+    ) {
+        logger.info { "[addBasicInformation] basicInformation = $basicInformation" }
+    }
+
+    data class UnderstandByUriRequest(
+        val uri: String
+    )
+
+    data class LoginResponse(
+        val sessionId: String,
+    )
+
+    data class UserResponse(
+        val basicInformation: BasicInformation,
+        val imageUrls: List<String> = emptyList(),
+        val snsInformation: SnsInformation,
+    )
+
+    data class BasicInformation(
+        val name: String?,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+        val birthDay: LocalDateTime?,
+        val gender: UserInformation.Gender?,
+        val mbti: UserInformation.MBTI?,
+        val major: String?,
+    )
+
+    data class SnsInformation(
+        val instagramUserIds : List<String> = emptyList(),
+        val blogUserIds: List<String> = emptyList(),
+    )
+
+    companion object : Log
+
 }
