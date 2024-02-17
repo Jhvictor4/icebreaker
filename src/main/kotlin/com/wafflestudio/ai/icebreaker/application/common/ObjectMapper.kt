42113994b5e59e77c12a525077bf741cccdd6932
강지혁 <wlgur7238@snu.ac.kr>
2024-02-18 01:45:51 +0900

    unknown properties fix

--- src/main/kotlin/com/wafflestudio/ai/icebreaker/application/common/ObjectMapper.kt
+++ src/main/kotlin/com/wafflestudio/ai/icebreaker/application/common/ObjectMapper.kt
@@ -1,7 +1,9 @@
 package com.wafflestudio.ai.icebreaker.application.common
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 val ObjectMapper = jacksonObjectMapper()
     .registerModules(JavaTimeModule())
+    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
